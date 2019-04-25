9f81565099e632db613ded7fba65a69031be8f6e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 19:30:03 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -10,7 +10,7 @@ export default class EyesOnMe extends React.Component {
   render() {
     return (
       <button
-      onFocus={console.log("Good!")}
+      onFocus={this.onFocus}
       onBlur={console.log("Hey! Eyes on me!")}
       >
       Eyes on me, please!
